a544bd78caae960015246d7f4ad130b978f4d155
zah-zah <mdzaheer4231@gmail.com>
2023-08-29 15:22:20 +0530

    second

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to DIET college.......!" );
+        System.out.println( "ZAHEER" );
     }
 }
